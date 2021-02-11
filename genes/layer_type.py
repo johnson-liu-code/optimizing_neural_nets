bf63ce089c05d2b6c53a522f20e27eb8156f15a0
Johnson Liu <j_liu21@u.pacific.edu>
2021-02-10 17:25:48 -0800

    20210210 Forgot to push changes last week. Started to add more layer parameters.

--- genes/layer_type.py
+++ genes/layer_type.py
@@ -16,6 +16,16 @@ layers_with_activation = [0, 1, 2, 3]
 layers_with_dimensionality = [0, 1, 2]
 layers_with_pooling = [4, 5]
 layers_needing_strides = [1, 2, 3, 4, 5]
+layers_with_kernel_regularizer = [0, 1]
+layers_with_kernel_constraint = [0, 1]
+layers_with_bias_constraint = [0, 1, 2, 3]
+layers_with_dilation_rate = [1, 2, 3]
+layers_with_groups = [1]
+layers_with_depth_multiplier = [2, 3]
+layers_with_depthwise_initializer = [2, 3]
+#layers_with_pointwise_initializer = [2]
+layers_with_depthwise_constraint = [2, 3]
+#layers_with_pointwise_constraint = [2]
 
 # keras.layers.Dense(units, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)
 # keras.layers.Conv2D(filters, kernel_size, strides=(1, 1), padding='valid', data_format=None, dilation_rate=(1, 1), activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)
